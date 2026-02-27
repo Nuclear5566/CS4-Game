04752d65c6f67e9cf1a8d254abc903130b65ec83
Amrmo <Amrmo@10.2.0.2>
2026-02-27 23:57:59 +0200

    Fein

---
+++ src/game/engine/Constants.java
@@ -0,0 +1,5 @@
+package game.engine;
+
+final public class Constants {
+
+}
---
+++ src/game/tests/PLACEHOLDER.java
@@ -0,0 +1,5 @@
+package game.tests;
+
+public class PLACEHOLDER {
+	/* PLACE HOLDER CLASS, WILL BE REPLACED WHEN WE GET TESTS*/
+}
