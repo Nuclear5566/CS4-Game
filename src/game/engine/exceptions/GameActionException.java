46e5192b91b66b5e985c01fad166b0a70782dead
Omar Rady <Omar Rady@BT-7274>
2026-03-02 20:10:38 +0200

    Completed the game.engine.exceptions (Except the csv part)

--- src/game/engine/exceptions/GameActionException.java
+++ src/game/engine/exceptions/GameActionException.java
@@ -1,5 +1,12 @@
 package game.engine.exceptions;
-
-public abstract class GameActionException {
-
+public abstract class GameActionException extends Exception {
+	private static final long serialVersionUID = 1L;
+	//This was for the error below
+	//The serializable class GameActionException does not declare a static final serialVersionUID field of type long
+	public GameActionException() {
+		super();
+	}
+	public GameActionException(String message) {
+		super(message);
+	}
 }
