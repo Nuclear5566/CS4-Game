029e37ee83f94fe7275d2c8cb77f7faa1163ef13
Nuclear5566 <amrmohdmoss@gmail.com>
2026-03-06 17:39:43 +0200

    Added more fixed for testing, currently tests run

--- src/game/engine/Game.java
+++ src/game/engine/Game.java
@@ -1,5 +1,18 @@
 package game.engine;
-
+import java.util.ArrayList;
+import game.engine.monsters.*;
+import java.io.*; //for IOException
 public class Game {
-
+	Board Board;
+	ArrayList<Monster> allMonsters;
+	Monster player;
+	Monster opponent;
+	Monster current;
+	
+	public Game(Role playerRole) throws IOException{
+		//requires CSV files
+	}
+	/*private Monster selectRandomMonsterByRole(Role role){
+		
+	}*/
 }
