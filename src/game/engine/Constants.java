f8130cfb2615c9e1f17ffe85aa4764b8fbc579b4
Nuclear5566 <amrmohdmoss@gmail.com>
2026-03-09 20:06:38 +0200

    fixed constants and added InvalidCSVFormat.java

--- src/game/engine/Constants.java
+++ src/game/engine/Constants.java
@@ -1,21 +1,21 @@
 package game.engine;
 
 final public class Constants {
-	public static int BOARD_SIZE = 100;
-	public static int BOARD_ROWS = 10;
-	public static int BOARD_COLS = 10;
-	public static int WINNING_POSITION = 99;
-	public static int STARTING_POSITION = 0;
-	public static int[] MONSTER_CELL_INDICES = {2, 18, 34, 54, 82, 88};
-	public static int[] CONVEYOR_CELL_INDICES = {6, 22, 44, 52, 66}; // start of the belt
-	public static int[] SOCK_CELL_INDICES = {32, 42, 74, 84, 98}; // start of the sock
-	public static int[] CARD_CELL_INDICES = {4, 12, 28, 36, 48, 56, 60, 76, 86, 90};
-	public static int WINNING_ENERGY = 1000;
-	public static int MIN_ENERGY = 0;
-	public static int MULTITASKER_BONUS = 200;
-	public static int SCHEMER_STEAL = 10;
-	public static int SLIP_PENALTY = 100;
-	public static int POWERUP_COST = 500;
+	final public static int BOARD_SIZE = 100;
+	final public static int BOARD_ROWS = 10;
+	final public static int BOARD_COLS = 10;
+	final public static int WINNING_POSITION = 99;
+	final public static int STARTING_POSITION = 0;
+	final public static int[] MONSTER_CELL_INDICES = {2, 18, 34, 54, 82, 88};
+	final public static int[] CONVEYOR_CELL_INDICES = {6, 22, 44, 52, 66}; // start of the belt
+	final public static int[] SOCK_CELL_INDICES = {32, 42, 74, 84, 98}; // start of the sock
+	final public static int[] CARD_CELL_INDICES = {4, 12, 28, 36, 48, 56, 60, 76, 86, 90};
+	final public static int WINNING_ENERGY = 1000;
+	final public static int MIN_ENERGY = 0;
+	final public static int MULTITASKER_BONUS = 200;
+	final public static int SCHEMER_STEAL = 10;
+	final public static int SLIP_PENALTY = 100;
+	final public static int POWERUP_COST = 500;
 
 
 	 
--- src/game/engine/exceptions/InvalidCSVFormat.java
+++ src/game/engine/exceptions/InvalidCSVFormat.java
@@ -1,5 +1,18 @@
 package game.engine.exceptions;
+import java.io.*;
 
-public class InvalidCSVFormat {
-
+public class InvalidCSVFormat extends GameActionException {
+	static final String MSG = "Invalid input detected while reading csv file, input = \n";
+	String inputLine;
+	
+	public InvalidCSVFormat(String inputLine) {
+		super(MSG);
+	}
+	public InvalidCSVFormat(String message, String inputLine){
+		super(inputLine);
+	}
+	
+	public void setInputLine(String inputLine) {
+		this.inputLine = inputLine;
+	}
 }
