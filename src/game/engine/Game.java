d65ffdcdc24080b827aace40f76379642feb485f
Nuclear5566 <amrmohdmoss@gmail.com>
2026-05-06 21:46:26 +0300

    milestone 2 shit ion fucking know mane

--- src/game/engine/Game.java
+++ src/game/engine/Game.java
@@ -9,6 +9,8 @@ import game.engine.dataloader.DataLoader;
 import game.engine.exceptions.InvalidMoveException;
 import game.engine.exceptions.OutOfEnergyException;
 import game.engine.monsters.*;
+import javafx.*;
+
 
 public class Game {
 	private Board board;
@@ -114,6 +116,9 @@ public class Game {
 			return opponent;
 		
 		return null;
+	}
+	public static void main(String [] args) {
+		
 	}
 	
 }
